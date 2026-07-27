97c173ff1247ad81afb96e8a644622ef8d0e555c
Dos2164BMT <dosprazerez@gmail.com>
2026-07-26 23:11:58 -0400

    Module 4: app_server/controllers/travel.js

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,16 +1,19 @@
-const home = (req, res) => {
-  res.render('index', {
-    title: 'Travlr Getaways'
-  });
-};
+const Trip = require('../../app_api/models/trip');
 
-const travel = (req, res) => {
-  res.render('travel', {
-    title: 'Travlr Getaways - Travel'
-  });
+const travel = async (req, res, next) => {
+  try {
+    const trips = await Trip.find({}).sort({ code: 1 }).lean().exec();
+
+    return res.render('travel', {
+      title: 'Travlr Getaways - Travel',
+      activeTravel: true,
+      trips
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
 
 module.exports = {
-  home,
   travel
 };
